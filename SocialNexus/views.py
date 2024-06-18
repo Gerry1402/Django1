2c19fc6a557fbdcfa21493497150f00124bfcb01
Gerry1402 <gerard.vello@gmail.com>
2024-06-18 11:32:03 +0200

    mensaje error ignorado

--- SocialNexus/views.py
+++ SocialNexus/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse # type: ignore
-from django.shortcuts import render
+from django.shortcuts import render # type: ignore
 
 def saludo(request):
     nombre = 'Gerard'
